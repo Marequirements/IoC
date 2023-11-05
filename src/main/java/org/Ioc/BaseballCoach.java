39070941d23088a28b2aa8a245f132528c56131c
Marek Lescinsky <marek.lescinsky@kosickaakademia.sk>
2023-11-05 22:41:54 +0100

    create ioc demo

---
+++ src/main/java/org/Ioc/BaseballCoach.java
@@ -0,0 +1,10 @@
+package org.Ioc;
+
+public class BaseballCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout(){
+        return "spend 30 minutes on batting practice";
+    }
+
+}
